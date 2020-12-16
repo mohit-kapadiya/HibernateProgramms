5d7bb5781cad264968945d92f8b2ebf2706b27f0
Mohit14-java <mohitkapadiya1408@gmail.com>
2020-12-16 19:23:28 +0530

    add some new concept or annotation in entity class

--- src/main/java/com/embedding/Employee.java
+++ src/main/java/com/embedding/Employee.java
@@ -1,11 +1,12 @@
 package com.embedding;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
 
 @Entity
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
